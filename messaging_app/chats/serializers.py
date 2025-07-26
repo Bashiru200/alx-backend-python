e59c94b8cd6e1f8cde547c729e8957f846c82a4e
Bashiru200 <bashirubarrie12@gmail.com>
2025-07-26 00:04:45 -0400

    views.py added

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.serializers import CharField, ValidationError
+
 from .models import Users, Message, Conversation
 
 class UserSerializer(serializers.ModelSerializer):
@@ -13,6 +15,29 @@ class UserSerializer(serializers.ModelSerializer):
             "role",
             "created_at",
         ]
+from rest_framework import serializers
+from rest_framework.serializers import CharField, ValidationError
+from .models import Users
+
+
+class UserSerializer(serializers.ModelSerializer):
+    first_name = CharField(max_length=100)
+
+    class Meta:
+        model = Users
+        fields = [
+            'user_id',
+            'first_name',
+            'last_name',
+            'email',
+            'phone_number',
+            'role',
+            'created_at',
+        ]
+    def validate_first_name(self, value):
+        if any(char.isdigit() for char in value):
+            raise ValidationError("First and Last name cannot use number.")
+        return value
 
 class MessageSerializer(serializers.ModelSerializer):
     sender = UserSerializer(read_only=True)
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from .models import Conversation, Message
+from .serializers import ConversationSerailizer, MessageSerializer
 
 # Create your views here.
+class ConversationViewSets(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all().prefetch_related('message', 'participant')
+    serializer_class = ConversationSerailizer
+
+    def create(self, request, *args, **kwargs):
+        # create a conversation with participant
+        participants_id = request.data.get('participants')
+        if not participants_id or not isinstance(participants_id, list):
+            return Response({"error: participant must be a list of user IDs"},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
+        conversation = conversation.objects.create()
+        conversation.participants.set(participants_id)
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class MessageViewSets(viewsets.ModelViewSet):
+    queryset = Message.objects.select_related('sender', 'conversation').all()
+    serializer_class = MessageSerializer
+
+    def  create(self, request, *args, **kwargs):
+        #send message to an existing conversation
+        conversation_id = request.data.get('conversation')
+        sender_id = request.data.get('sender')
+        body = request.data.get('message_body')
+
+        if not (conversation_id and sender_id and body):
+            return Response({"error": "Missing required fields."},
+                            status=status.HTTP_400_BAD_REQUEST)
+        
+
+        try:
+            conversation = Conversation.objects.get(pk=conversation_id)
+        except Conversation.DoesNotExist:
+            return Response({"error": "Conversation not found."},
+                            status=status.HTTP_404_NOT_FOUND)
+
+        message = Message.objects.create(
+            sender_id=sender_id,
+            conversation=conversation,
+            message_body=body
+        )
+        serializer = self.get_serializer(message)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
