f55553f6af556533fc42e9431b03f7d8a5d52bda
Bashiru200 <bashirubarrie12@gmail.com>
2025-07-27 14:19:21 -0400

    enabling the nested function

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -26,32 +26,42 @@ class ConversationViewSets(viewsets.ModelViewSet):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class MessageViewSets(viewsets.ModelViewSet):
-    queryset = Message.objects.select_related('sender', 'conversation').all()
+    # queryset = Message.objects.select_related('sender', 'conversation').all()
     serializer_class = MessageSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['participants__user_id']
+#     filter_backends = [filters.SearchFilter]
+#     search_fields = ['participants__user_id']
 
-    def  create(self, request, *args, **kwargs):
-        #send message to an existing conversation
-        conversation_id = request.data.get('conversation')
-        sender_id = request.data.get('sender')
-        body = request.data.get('message_body')
+#     def  create(self, request, *args, **kwargs):
+#         #send message to an existing conversation
+#         conversation_id = request.data.get('conversation')
+#         sender_id = request.data.get('sender')
+#         body = request.data.get('message_body')
 
-        if not (conversation_id and sender_id and body):
-            return Response({"error": "Missing required fields."},
-                            status=status.HTTP_400_BAD_REQUEST)
+#         if not (conversation_id and sender_id and body):
+#             return Response({"error": "Missing required fields."},
+#                             status=status.HTTP_400_BAD_REQUEST)
         
 
-        try:
-            conversation = Conversation.objects.get(pk=conversation_id)
-        except Conversation.DoesNotExist:
-            return Response({"error": "Conversation not found."},
-                            status=status.HTTP_404_NOT_FOUND)
+#         try:
+#             conversation = Conversation.objects.get(pk=conversation_id)
+#         except Conversation.DoesNotExist:
+#             return Response({"error": "Conversation not found."},
+#                             status=status.HTTP_404_NOT_FOUND)
 
-        message = Message.objects.create(
-            sender_id=sender_id,
-            conversation=conversation,
-            message_body=body
-        )
-        serializer = self.get_serializer(message)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         message = Message.objects.create(
+#             sender_id=sender_id,
+#             conversation=conversation,
+#             message_body=body
+#         )
+#         serializer = self.get_serializer(message)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+# #NEWLINE
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get('conversation_pk')
+        return Message.objects.filter(conversation_id=conversation_id)
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get('conversation_pk')
+        serializer.save(conversation_id=conversation_id)
