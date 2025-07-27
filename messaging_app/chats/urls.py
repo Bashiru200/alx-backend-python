4d0ee5143efba84fdeda7d9e6ee4968ea076ab6b
Bashiru200 <bashirubarrie12@gmail.com>
2025-07-27 14:09:49 -0400

    nestedDefauldrouter added

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import ConversationViewSet, MessageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
-router.register(r'messages', MessageViewSet, basename='message')
+
+#Nested router for messages
+conversation_router = routers.NestedDefaultRouter(router, r'conversation', lookup='converstaion')
+conversation_router.regiter(r'messages', MessageViewSet, basename='conversation-messages')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(conversation_router.urls))
 ]
