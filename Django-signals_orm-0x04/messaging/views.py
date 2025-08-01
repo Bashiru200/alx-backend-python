9be318c47b0a56d2ff34c0743a8227770cb66232
Bashiru200 <bashirubarrie12@gmail.com>
2025-07-31 22:56:18 -0400

    debugging

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -28,12 +28,49 @@
 #     unread_messages = UnreadMessagesManager().get_queryset()
 #     return render(request, 'chat/unread_messages.html', {'unread_messages': unread_messages})
 
+# from django.contrib.auth.decorators import login_required
+# from django.contrib import messages
+# from django.contrib.auth import get_user_model
+# from django.shortcuts import redirect, render
+# from .models import Message
+# from .utils import build_threaded  # fix spelling and import appropriately
+# from django.views.decorators.cache import cache_page
+
+# User = get_user_model()
+
+# @login_required
+# def delete_user(request):
+#     user = request.user
+#     user.delete()
+#     messages.success(request, "Your account and all associated data have been deleted.")
+#     return redirect('home')
+
+# @login_required
+# def conversation_view(request, conversation_owner):
+#     root_msgs = Message.objects.filter(receiver=conversation_owner, parent_message__isnull=True) \
+#         .select_related('sender', 'receiver') \
+#         .prefetch_related('replies__sender', 'replies__receiver', 'replies__replies')
+#     thread = build_threaded(root_msgs)
+#     return render(request, 'chat/threaded.html', {'thread': thread})
+
+# @login_required
+# def unreadmessages(request):
+#     unread_messages = Message.unread.unread_for_user  # Make sure the manager is defined on the model
+#     return render(request, 'chat/unread_messages.html', {'unread_messages': unread_messages})
+
+# # cache_page decorator to cache the response for 60 seconds
+# @cache_page(60)
+# def convrsation_messages(request, conversation_pk):
+#     #fetch messages for a specific conversation
+#     message = Message.objects.filter(conversation_id=conversation_pk).order_by('sent_at')
+#     return render(request, 'conversations/messages.html', {'messages': message})
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.shortcuts import redirect, render
 from .models import Message
-from .utils import build_threaded  # fix spelling and import appropriately
+from .utils import build_threaded
 from django.views.decorators.cache import cache_page
 
 User = get_user_model()
@@ -45,22 +82,37 @@ def delete_user(request):
     messages.success(request, "Your account and all associated data have been deleted.")
     return redirect('home')
 
+
 @login_required
 def conversation_view(request, conversation_owner):
+    if request.method == 'POST':
+        # Create and send a new message
+        content = request.POST.get('content')
+        if content:
+            Message.objects.create(
+                sender=request.user,
+                receiver=conversation_owner,
+                content=content
+            )
+            messages.success(request, "Message sent.")
+            return redirect('conversation_view', conversation_owner=conversation_owner)
+
     root_msgs = Message.objects.filter(receiver=conversation_owner, parent_message__isnull=True) \
         .select_related('sender', 'receiver') \
         .prefetch_related('replies__sender', 'replies__receiver', 'replies__replies')
     thread = build_threaded(root_msgs)
     return render(request, 'chat/threaded.html', {'thread': thread})
 
+
 @login_required
 def unreadmessages(request):
-    unread_messages = Message.unread.unread_for_user  # Make sure the manager is defined on the model
+    sender = request.user
+    unread_messages = Message.unread.unread_for_user(sender)
     return render(request, 'chat/unread_messages.html', {'unread_messages': unread_messages})
 
-# cache_page decorator to cache the response for 60 seconds
+
+@login_required
 @cache_page(60)
-def convrsation_messages(request, conversation_pk):
-    #fetch messages for a specific conversation
-    message = Message.objects.filter(conversation_id=conversation_pk).order_by('sent_at')
+def conversation_messages(request, conversation_pk):
+    messages = Message.objects.filter(conversation_id=conversation_pk).order_by('sent_at')
     return render(request, 'conversations/messages.html', {'messages': messages})
